e47a420390f6b2ca81e04c32491b49c46c76cd6c
michaelbag <root@rr-srv.rhana.local>
2020-01-30 13:59:39 +0300

    Added some information

--- dump_db.sh
+++ dump_db.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Bash scripte for custom format backup of postgresql database.
+#
+# Author: Mikhail Kudryavtsev
+#
+# * Don't forget in user home folder should be .pgpass file
+#  in format like:
+#
+#<hostname>:<port>:<database>:<username>:<password>
+#
+# Read https://wiki.postgresql.org/wiki/Pgpass for more information.
+
 if [ $# -ne 3 ]; then
   echo "usage: dumb_db <username> <db_name> <dump_file>"
   exit 1
